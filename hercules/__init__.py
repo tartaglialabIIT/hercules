400745e194075986545c8dc5c9bb88594c59f84d
jonathan-f <54023927+jonathan-f@users.noreply.github.com>
2026-01-22 12:37:29 +0100

    Update __init__.py

--- hercules/__init__.py
+++ hercules/__init__.py
@@ -1,13 +1,11 @@
 from .api import (
     profiles,
-    global_proteinbert_score,
     global_fused_score,
     mutagenesis,
 )
 
 __all__ = [
     "profiles",
-    "global_proteinbert_score",
     "global_fused_score",
     "mutagenesis",
 ]
