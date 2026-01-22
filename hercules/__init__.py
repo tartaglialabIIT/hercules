0a32e437690096164884e1682e025fafb7d0886e
jonathan-f <54023927+jonathan-f@users.noreply.github.com>
2026-01-22 12:56:18 +0100

    Update __init__.py

--- hercules/__init__.py
+++ hercules/__init__.py
@@ -1,3 +1,16 @@
+import warnings
+import os
+
+# -------------------------------
+# Suppress all Python warnings
+# -------------------------------
+warnings.filterwarnings("ignore")
+
+# -------------------------------
+# Optional: suppress TensorFlow logging
+# -------------------------------
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"  # 0=all, 1=info, 2=warnings, 3=errors only
+
 from .api import (
     profiles,
     global_fused_score,
